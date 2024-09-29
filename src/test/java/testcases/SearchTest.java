bfb28c2fbec96526b30a6c2b1bf99359d7f488cb
pourn <pourn@192.168.1.6>
2024-09-29 14:17:03 +0530

    dependency removed from search....

--- src/test/java/testcases/SearchTest.java
+++ src/test/java/testcases/SearchTest.java
@@ -55,6 +55,7 @@ public class SearchTest extends BaseClass{
 		
 	}
 /* (priority=3,dependsOnMethods= {"verifySearchWithInValidProduct"})*/
+	//
 	@Test (priority=3)
 	public void verifySearchWithoutAnyProduct()
 	{
